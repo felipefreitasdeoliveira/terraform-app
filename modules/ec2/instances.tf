1e505eb2a8a9c461f6885a9e501fbfc7fb106c5b
Felipe Freitas Oliveira <feloliveira@uoldiveo.com>
2020-09-15 18:17:24 -0300

    Ajustes na região

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
-  count         = "${var.ec2_count}"
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  subnet_id     = "${var.subnet_id}"
+  count         = var.ec2_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
 
-  tags {
+  tags = {
     Name = "HelloWorld"
   }
 }
--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -11,3 +11,7 @@ variable "vpc_id" {}
 variable "subnet_cidr" {
   default = "10.0.1.0/24"
 }
+
+variable "tags" {
+  default = "Main"
+}
